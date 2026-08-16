6f477155104c817d75449eea11794429b91cdbf7
mulheresparaestetempo <daianefeliciano.df@gmail.com>
2026-08-16 19:16:17 -0300

    Adicionar página de gerenciamento de podcasts

---
+++ apps/painel_lider/app/admin/recursos/podcasts/page.tsx
@@ -0,0 +1,2 @@
+import ResourceManager from '../ResourceManager';
+export default function PodcastsPage() { return <ResourceManager type="podcast" title="Podcasts" icon="🎙️" description="Cadastre séries, episódios e ensinamentos com links para Spotify, YouTube ou outros players." />; }
